6d967fb1818eceb84edc91d6ed20338f97c11103
papanda925 <papanda925@gmail.com>
2022-06-07 00:16:09 +0900

    Create WPF_GraphicalDatePickerSample.ps1

---
+++ WPF_GraphicalDatePickerSample.ps1
@@ -0,0 +1,57 @@
+#presentationframeworkを読み込み
+Add-Type -AssemblyName presentationframework
+
+#XAMLファイルを読み込んでxmlに変換
+[System.Xml.XmlDocument]$xaml = Get-Content .\WPF_GraphicalDatePickerSample.xaml
+$xaml.GetType().FullName
+#visual studio でデザインした場合に付与され、Powershellで読み込む時に不要な要素を削除
+$xaml.window.RemoveAttribute("x:Class")
+$xaml.window.RemoveAttribute("mc:Ignorable")
+
+#読み込んだxamlをXmlNodeReaderにキャスト
+[System.Xml.XmlNodeReader]$xamlReader = $xaml -as "System.Xml.XmlNodeReader"
+$xamlReader.GetType().FullName
+#Windows.Markup.XamlReaderで読み込み
+[System.Windows.Window]$mainWindow = [Windows.Markup.XamlReader]::Load($xamlReader)
+$mainWindow.gettype().FullName
+
+#各種コントロールを取得
+[System.Windows.Controls.Button]$okButton = $mainWindow.FindName("OKButton")
+$okButton.gettype().FullName
+[System.Windows.Controls.Button]$CancelButton = $mainWindow.FindName("CancelButton")
+$CancelButton.gettype().FullName
+#[System.Windows.Controls.TextBox]$TextBox = $mainWindow.FindName("TextBox")
+#$TextBox.gettype().FullName
+[System.Windows.Controls.Label]$Label = $mainWindow.FindName("Label")
+$Label.gettype().FullName
+[System.Windows.Controls.Calendar]$Calendar = $mainWindow.FindName("Calendar")
+$Calendar.gettype().FullName
+
+$Calendar.SelectedDate = [datetime]::Now
+$Label.Content =  $Calendar.SelectedDate.ToString("'Today:' yyyy/MM/dd")
+
+#イベントを追加
+$okButton.add_Click.Invoke({
+#    Write-Output "Using $textBox.Text" | Out-Host 
+    $mainWindow.DialogResult = $true
+    $mainWindow.Close()
+    return  
+})
+$CancelButton.add_Click.Invoke({
+    Write-Output "Cancel Click Bye" | Out-Host 
+    $mainWindow.DialogResult = $false
+    $mainWindow.Close()
+    return     
+})
+
+#表示
+#$mainWindow.showDialog() | out-null
+[System.Boolean]$result = $mainWindow.showDialog()
+$result.GetType().FullName
+Write-Output "Result is $result" | Out-Host 
+
+if ($result -eq $true )
+{
+    $x = $Calendar.SelectedDate.ToString()
+    $x
+}
